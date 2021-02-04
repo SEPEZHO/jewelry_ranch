522bd54b91b3cfec3b7a4b8c7bb50bd734c8bfc4
sepezho <sepezho@gmail.com>
2021-02-04 23:12:36 +0300

    Started a new project

---
+++ src/Components/MainPage/Text/Text.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import s from './Style/Text.module.sass';
+
+function Text() {
+  return (
+    <div className={s.TextContainer}>
+      <p>Привет, я jewelry_ranch.</p>
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> jewelry_ranch
+      jewelry_ranch <span className={s.coursive}>jewelry_ranch</span> jewelry_ranch <span className={s.secondaryText}>jewelry_ranch</span> 
+      <p>Удачи тебе :)</p>
+    </div>
+  );
+}
+
+export default Text;
